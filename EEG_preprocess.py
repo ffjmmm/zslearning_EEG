eeaa4c5011f96decc493675c7af17de30cb95d8e
Jieming Fan <ffjmmm@outlook.com>
2021-06-20 19:21:01 +0800

    update

---
+++ EEG_preprocess.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import ShuffleSplit, cross_val_score
+
+import mne
+from mne import Epochs, pick_types, events_from_annotations
+from mne.channels import make_standard_montage
+from mne.io import concatenate_raws, read_raw_edf
+from mne.datasets import eegbci
+from mne.decoding import CSP
+
+mne.set_log_file('./mne_output', overwrite=True)
+
+tmin, tmax = -1., 4.
+event_id = dict(left=2, right=3)
+subject = 1
+runs = [4, 8, 12]
+
+raw_fnames = eegbci.load_data(subject, runs, path='./data')
+raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
+eegbci.standardize(raw)  # set channel names
+montage = make_standard_montage('standard_1005')
+raw.set_montage(montage)
+
+# strip channel names of "." characters
+raw.rename_channels(lambda x: x.strip('.'))
+
+# Apply band-pass filter
+raw.filter(7., 30., fir_design='firwin', skip_by_annotation='edge')
+
+# events, _ = events_from_annotations(raw, event_id=dict(T0=1, T1=2, T2=3))
+events, _ = events_from_annotations(raw, event_id=dict(T1=2, T2=3))
+
+picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
+                   exclude='bads')
+
+# Read epochs (train will be done only between 1 and 2s)
+# Testing will be done with a running classifier
+epochs = Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
+                baseline=None, preload=True)
+print(epochs)
+exit()
+epochs_train = epochs.copy().crop(tmin=1., tmax=2.)
+labels = epochs.events[:, -1] - 2
+
+
+# Define a monte-carlo cross-validation generator (reduce variance):
+scores = []
+epochs_data = epochs.get_data()
+epochs_data_train = epochs_train.get_data()
+cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+cv_split = cv.split(epochs_data_train)
+
+# Assemble a classifier
+lda = LinearDiscriminantAnalysis()
+csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
+
+# Use scikit-learn Pipeline with cross_val_score function
+clf = Pipeline([('CSP', csp), ('LDA', lda)])
+scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=1)
+
+# Printing the results
+class_balance = np.mean(labels == labels[0])
+class_balance = max(class_balance, 1. - class_balance)
+print("Classification accuracy: %f / Chance level: %f" % (np.mean(scores),
+                                                          class_balance))
+
+# plot CSP patterns estimated on full data for visualization
+csp.fit_transform(epochs_data, labels)
+
+csp.plot_patterns(epochs.info, ch_type='eeg', units='Patterns (AU)', size=1.5)
+
+
+sfreq = raw.info['sfreq']
+w_length = int(sfreq * 0.5)   # running classifier: window length
+w_step = int(sfreq * 0.1)  # running classifier: window step size
+w_start = np.arange(0, epochs_data.shape[2] - w_length, w_step)
+
+scores_windows = []
+
+for train_idx, test_idx in cv_split:
+    y_train, y_test = labels[train_idx], labels[test_idx]
+
+    X_train = csp.fit_transform(epochs_data_train[train_idx], y_train)
+    X_test = csp.transform(epochs_data_train[test_idx])
+
+    # fit classifier
+    lda.fit(X_train, y_train)
+
+    # running classifier: test classifier on sliding window
+    score_this_window = []
+    for n in w_start:
+        X_test = csp.transform(epochs_data[test_idx][:, :, n:(n + w_length)])
+        score_this_window.append(lda.score(X_test, y_test))
+    scores_windows.append(score_this_window)
